1957456c981555f46371d36befb8e31598a568d5
Ryan Feng <odayfans@gmail.com>
2014-07-05 06:50:56 -0400

    Add deploy.sh, separate dev/deploy configs in uwsgi

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+usage(){
+    echo "usage:$0 [package|unpackage <file>|clean|start [dev|deploy] [ini file]|reload|stop]"
+}
+
+check_deploy_env(){
+    if [ -z "$DEPLOY" ];then
+        echo "Not in deployment environment. Exiting..."
+        exit 2
+    fi
+}
+
+main(){
+    case "$1" in
+        package)
+            PACKAGE_DIR=packages
+            PACKAGE_NAME=`date +%F_%H-%M-%S`.tar.gz
+            PACKAGE_PATH=$PACKAGE_DIR/$PACKAGE_NAME
+            [ ! -d ./packages ] && mkdir $PACKAGE_DIR
+            tar zvc ./deploy.sh ./imods ./uwsgi.ini -f $PACKAGE_PATH
+            echo $PACKAGE_PATH
+            ;;
+        unpackage)
+            if [ -z "$2" ];then
+                usage
+                return 1
+            fi
+            check_deploy_env
+            tar zxv -C ~/app -f $2
+            ;;
+        clean)
+            check_deploy_env
+            rm -rf packages
+            rm -rf ~/app/*
+            ;;
+        start)
+            uwsgi --ini ${3-uwsgi.ini}:${2-dev}
+            ;;
+        reload)
+            if [ ! -r /tmp/uwsgi_imods.pid ];then
+                echo "uwsgi not started"
+                return 0
+            fi
+            uwsgi --reload /tmp/uwsgi_imods.pid
+            ;;
+        stop)
+            pkill -INT uwsgi
+            ;;
+        *) usage;;
+    esac
+}
+
+main $*
