c633b292676d9bc766fcd5158e827ea0accfdb89
Ryan Feng <odayfans@gmail.com>
2014-07-09 03:39:16 -0400

    Move schemas in the root directory

--- schemas/create.sql
+++ schemas/create.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS "USER"(
     "uid" INT NOT NULL ,
     "email" VARCHAR(200) NOT NULL UNIQUE,
     "password" VARCHAR(200) NOT NULL,
-    "first_name" VARCHAR(50) NOT NULL,
-    "last_name" VARCHAR(50) NOT NULL,
+    "fullname" VARCHAR(50) NOT NULL,
     "age" INT NOT NULL,
     "address" VARCHAR(200) NOT NULL,
     "author_identifier" CHAR(200) NOT NULL, -- author identifier for app, and any other contents
