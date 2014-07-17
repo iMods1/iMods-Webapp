4be2888d3ec44fa30e7540c1d14ed31dab7e795c
Ryan Feng <odayfans@gmail.com>
2014-07-16 21:00:38 -0400

    Update CI testing

--- imods/__init__.py
+++ imods/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import config
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(config)
 
 db = SQLAlchemy(app)
 
