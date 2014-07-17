8f2ad251137e91bf77c6165586e925cd0bda98b1
Ryan Feng <odayfans@gmail.com>
2014-07-16 21:06:13 -0400

    Update CI

--- imods/__init__.py
+++ imods/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-import os
-os.path.append('')
+import sys
+sys.path.append('')
 
 app = Flask(__name__)
 app.config.from_object('config')
